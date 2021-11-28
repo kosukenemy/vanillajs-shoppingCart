1a6e1e51b7cd224a5a2131ea026323b98ffcbd79
kosukenemy <kosukenemy@gmail.com>
2021-11-28 11:44:04 +0900

    update: デザイン追加

--- js/index.js
+++ js/index.js
@@ -1,51 +1,75 @@
 
 const productAPI = "https://www.product_countjsonapi.com/products/";
+const myCartCountElement =　document.querySelector('.js-myCart-count');
+const header = document.querySelector('.header');
+const cartPriceElement = document.querySelector('.js-cartPrice');
+const cartOpenUpElement = document.querySelector('.section');
+const cartIcon = document.querySelector('.cartIcon');
+const cart = document.querySelector('.cartWrapper');
+const closeBtn = document.querySelector('.closeBtn');
+
 
 
 class ShoppingCart {
   constructor(product){
     this.cart = [];
-    this.mathTotal = [];
+    this.cartTotal = [];
+    this.cartLength = [];
     this.product = product;
     this.productCount = 1;
   }
   init(){
     this.cart = [];
-    this.mathTotal = [];
+    this.cartTotal = [];
   }
   addItem(event){
     const thisItem = this.product.find(item => item.id === event.currentTarget.id);
     thisItem["product_count"] = this.productCount;
+    alert(thisItem.product_title +'が' + thisItem.product_count + "個がカートに追加されました");
 
     if (this.cart.indexOf(thisItem) !== -1) return thisItem;
     this.cart.push(thisItem);
   }
   removeItem(elementIndex){
+    const removeThisItem = this.product.find(item => item.id === elementIndex);
     this.cart = this.cart.filter(item => { return item.id !== elementIndex });
+    alert(removeThisItem.product_title +　"がカートから削除されました");
   }
   getTotalSum(){
     this.cart.map(item => {
       const withoutDollars = item.product_price.split("$").join('');
-      this.mathTotal.push( Number(withoutDollars) * item.product_count );
+      this.cartTotal.push( Number(withoutDollars) * item.product_count );
     });
 
-    const math = this.mathTotal.reduce((sum, i) => { return sum + i; }, 0);
-    console.log(this.mathTotal, "合計");
+    const math = this.cartTotal.reduce((sum, i) => { return sum + i; }, 0);
 
-    this.mathTotal = [];
-    const cartPrice = document.querySelector('.js-cartPrice');
-    if ( this.cart.length === 0 ) {
-      return cartPrice.innerHTML = [];
-    }
-    return cartPrice.innerHTML = wrapperCartTotal(math);
+    this.cartTotal = [];
+    
+    const cartLength = this.cart.map(item => {
+      return [item.product_count].reduce((sum, i) => {
+        return [sum, i];
+      });
+    });
+    
+    if ( this.cart.length === 0 ) { 
+      cartPriceElement.innerHTML = "";
+      myCartCountElement.textContent = 0;
+      return false;
+    };
+    const myCartCount = cartLength.reduce((sum, i) => { return sum + i } );
+
+    cartPriceElement.innerHTML = wrapperCartTotal(math);
+    myCartCountElement.textContent = myCartCount;
+    return;
   }
 }
 
+
 const wrapperCartTotal = (totalPrice) => {
   return [
-    '<div>',
-      '<span>total: </span>',
-      '<span>',
+    '<div class="price">',
+      '<span class="price-title">total: $</span>',
+      '<span class="price-total">',
         totalPrice,
       '</span>',
     '</div>'
@@ -59,39 +83,34 @@ shoppingCart.init();
 
 // template
 document.addEventListener('DOMContentLoaded', () => {
-  // template要素からコンテンツを取得、
   const content = document.querySelector('#imageTemplate').content;
-  // フラグメント
   const fragment = document.createDocumentFragment();
 
   for (const data of productsItems ) {
-    // テンプレートのノードを複製
     const clone = document.importNode(content, true);
 
-    //テンプレート内の要素
     const li = clone.querySelector('li');
     const button = clone.querySelector('button');
     const title = clone.querySelector('.item-title');
     const image = clone.querySelector('.item-thumbnail img');
     const description = clone.querySelector('.item-description');
-    const removes = clone.querySelector('.js-removeBtn');
+    const price = clone.querySelector('.item-price');
 
-    // テンプレートの要素に適用する
     li.id = data.id;
     button.id = data.id;
     image.src = data.product_image;
     image.alt = data.product_title;
     title.textContent = data.product_title;
+    price.textContent = data.product_price;
     description.textContent = data.product_description;
 
-    // 複製したノードをフラグメントに挿入
     fragment.appendChild(clone);
   }
 
-  // HTMLに挿入
   document.querySelector('#productItems').appendChild(fragment);
   viewModel();
 });
+
 
 const viewModel = () => {
   const InitialState = {
@@ -99,7 +118,6 @@ const viewModel = () => {
   }
   const addButtons = document.querySelectorAll('.js-addBtn');
   const counts = document.querySelectorAll('.js-select');
-
 
   addButtons.forEach(addButton => {
     addButton.addEventListener('click', (event) => {
@@ -110,11 +128,12 @@ const viewModel = () => {
     })
   });
 
+  cartIcon.addEventListener('click', () => toggleCart(cart, "is-open", header , "is-up") );
+  closeBtn.addEventListener('click', () => toggleCart(cart, "is-open", header , "is-up") );
 
   counts.forEach(count => {
     count.addEventListener('change', (event) => {
-      let inputNumber = event.currentTarget.value;
-      shoppingCart.productCount = Number(inputNumber);
+      shoppingCart.productCount = Number(event.currentTarget.value);
     })
   });
 }
@@ -146,19 +165,22 @@ const openCart = (initialState) => {
   }
   
   const cartItemsWrap = document.querySelector('#cartItems');
-  const clear = "";
-  cartItemsWrap.innerHTML = clear;
+  cartItemsWrap.innerHTML = [];
   cartItemsWrap.appendChild(cartFragment);
 }
 
+const toggleCart = (cartElement, openClass, header, upClass) => {
+  cartElement.classList.toggle(openClass);
+  header.classList.toggle(upClass);
+}
+
 document.addEventListener('click', (event) => {
-  // remove
   if ( event.target && event.target.dataset.item === "remove" ) {
     event.target.closest('.cart-item').remove();
     shoppingCart.removeItem(event.target.id);
     shoppingCart.getTotalSum();
-  }
-})
+  }  
+});
 
 
 
